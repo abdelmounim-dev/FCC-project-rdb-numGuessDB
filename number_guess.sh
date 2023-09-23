3cd706f499115dc4504e1ef0745c6fa9087d3359
abdelmounim-dev <77006851+abdelmounim-dev@users.noreply.github.com>
2023-09-23 09:46:26 -0400

    feat: query user and echo welcome message

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+# randomly generate a number that users have to guess
+
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+NUM=$(( $RANDOM % 1000 + 1))
+
+
+
+echo -e "\nEnter your username:"
+read USERNAME
+# check if user already exists
+USER_RESULT=$($PSQL "select username, num_games, best_game from users where username = $USERNAME")
+if [[ -z $USER_RESULT ]]
+then 
+	INSERT_USER_RESULT=$($PSQL "insert into users (username) values ('$USERNAME')")
+fi
